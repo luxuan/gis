9f0678432a27899ac896419fb4c8bf3a307a4a88
luxuan <hduliubin@qq.com>
2013-04-03 16:48:20 +0800

    cache baseLayer

--- app/BaseLayer.js
+++ app/BaseLayer.js
@@ -6,59 +6,98 @@ dojo.declare(
         _url: "",
         _color: "#ffff00",
         _graphics: [],
+        _level: -1,
+        _xCenter: -1,
+        _yCenter: -1,
+        _isShow: false,
+        _isUpdate: false,
         
         constructor: function(map, url, color) {
             this._map = map;
             this._url = url;
             this._color = color;
+            this._isUpdate = true;
         },
 
-        hide: function() {
-            var map = this._map;
-            dojo.forEach(this._graphics, function(entry, i){
-                map.graphics.remove(entry);
-            });
+        update: function(level, xCenter, yCenter){
+            this._level = level;
+            this._xCenter = xCenter;
+            this._yCenter = yCenter;
+            this._isUpdate = true;
+            if(this._isShow) { //刷新显示
+                this.hide();
+                this.show();
+            }
         },
-        show: function() {
-            var symbol = new esri.symbol.SimpleMarkerSymbol();
-            symbol.setColor(new dojo.Color(this._color));
-            console.log('_url', this._url);
-            console.log('_color', this._color);
-            console.log('this', this);
-            var store = new dojo.data.ItemFileReadStore({ url: this._url });
-            var graphics = this._graphics;
-            var map = this._map;
-            var gotList = function(items, request) {
-                for (var i = 0; i < items.length; i++) {
-                    // getValue用于得到某个item的某个属性名称的值
-                    var attr = {
-                        "name": store.getValue(items[i], "name"),
-                        "url": store.getValue(items[i], "url")
-                    };
-                    var loc = new esri.geometry.Point(store.getValue(items[i], "x"), store.getValue(items[i], "y"), { wkid: 4326 });
-                    var graphic = new esri.Graphic(loc, symbol, attr, new esri.InfoTemplate("${name}", "${url}"));
-                    graphics.push(graphic);
-                    map.graphics.add(graphic);
-                }
-            };
+
+        load: function(gotList) { //gotList = function(items, request)
+            //ItemFileReadStore有缓存，只要是使用过的url均从url中取
+            var url = this._url + '?level=' + this._level + '&x=' + this._xCenter + '&y=' + this._yCenter;
+            var store = new dojo.data.ItemFileReadStore({ url: url });
             // 自定义函数，用于异常处理
             var gotError = function(error, request) {
                 console.error("读文件请求失败！" + error);
             };
             store.fetch({ onComplete: gotList, onError: gotError, query: { id: "*"} });
-
-            dojo.connect(this._map.graphics, "onMouseOver", this.gOnMouseOverHandler);
-            dojo.connect(this._map.graphics, "onMouseOut", this.gOnMouseOutHandler);
-        },
-        
-        gOnMouseOutHandler: function(evt) {
-            this._map.infoWindow.hide();
         },
 
-        gOnMouseOverHandler: function(evt) {
-            this._map.infoWindow.setTitle(evt.graphic.attributes.name);
-            this._map.infoWindow.setContent(evt.graphic.attributes.url);
-            this._map.infoWindow.show(evt.screenPoint);
+        hide: function() {
+            this._isShow = false;
+            var map = this._map;
+            dojo.forEach(this._graphics, function(entry, i){
+                map.graphics.remove(entry);
+            });
+        },
+        loadedShow: function() {
+            var map = this._map;
+            dojo.forEach(this._graphics, function(entry, i){
+                map.graphics.add(entry);
+            });
+            this.addMouseListener();    //增加鼠标监听
+        },
+        show: function() {
+            this._isShow = true;
+            if(this._isUpdate) {
+                this._isUpdate = false;
+                var symbol = new esri.symbol.SimpleMarkerSymbol();
+                symbol.setColor(new dojo.Color(this._color));
+
+                var me = this;
+                var graphics = this._graphics;
+                var map = this._map;
+                var gotList = function(items, request) {
+                    //console.log('gotList');
+                    for (var i = 0; i < items.length; i++) {
+                        // getValue用于得到某个item的某个属性名称的值
+                        var attr = {
+                            "name": items[i].name[0],//store.getValue(items[i], "name"),
+                            "url": items[i].url[0]//store.getValue(items[i], "url")
+                        };
+                        var loc = new esri.geometry.Point(items[i].x[0], items[i].y[0], { wkid: 4326 });
+                        var graphic = new esri.Graphic(loc, symbol, attr, new esri.InfoTemplate("${name}", "${url}"));
+                        graphics.push(graphic);
+                    }
+                    me.loadedShow();
+                };
+                me.load(gotList);
+            }
+            else this.loadedShow();
+
+        },
+
+        addMouseListener: function() {
+            var map = this._map;
+            var gOnMouseOutHandler = function(evt) {
+                map.infoWindow.hide();
+            };
+            var gOnMouseOverHandler = function(evt) {
+                map.infoWindow.setTitle(evt.graphic.attributes.name);
+                map.infoWindow.setContent(evt.graphic.attributes.url);
+                map.infoWindow.show(evt.screenPoint);
+            };
+
+            dojo.connect(map.graphics, "onMouseOver", gOnMouseOverHandler);
+            dojo.connect(map.graphics, "onMouseOut", gOnMouseOutHandler);
         }
 
     }
